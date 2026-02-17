464ac73a63332a876069bd07816e2733484cc1ac
aleksivatj <avatjusa@gmail.com>
2026-02-17 13:43:01 +0200

    login lisäyksiä

--- app.js
+++ app.js
@@ -3,18 +3,31 @@ const app = express();
 const port = 3000;
 const productsRouter = require('./routes/products');
 const customersRouter = require('./routes/customers');
+const loginRouter = require('./routes/login');
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
 
 app.use(express.json());
+dotenv.config();
 
 app.get('/', function(request, response){
     response.send("Verkkokauppa API");
 });
 
+app.use('/login', loginRouter);
 app.use('/products', productsRouter);
 app.use('/customers', customersRouter);
 
 app.listen(port, function(){
     console.log("Palvelin kuuntelee porttia: "+ port);
 });
+
+function authenticateToken(request, response, next){
+    const authHeader = request.headers['authorization'];
+
+    if(!authheader) {
+        return response.sendStatus
+    }
+}
 
 module.exports = app;
