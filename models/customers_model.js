fa4dc50bfc68072c9e7f95c33555fb9a290b90de
aleksivatj <avatjusa@gmail.com>
2026-02-17 12:09:27 +0200

    customers taulun crud operaatiot valmiit

--- models/customers_model.js
+++ models/customers_model.js
@@ -3,6 +3,15 @@ const bcrypt = require('bcryptjs');
 const saltrounds = 12;
 
 const customers = {
+    getAllCustomers(callback){
+        return db.query("SELECT * FROM customers", callback);
+    },
+
+    getOneCustomer(un, callback){
+        return db.query("SELECT * FROM customers WHERE username = ?",
+            [un], callback);
+    },
+
     add(newC, callback){
         bcrypt.hash(newC.password, saltrounds, function(err, hashedPassword){
             if(err){
@@ -19,6 +28,32 @@ const customers = {
         });
 
     },
+
+    update(un, newC, callback){
+        bcrypt.hash(newC.password, saltrounds, function(err, hashedPassword){
+            if(err) {
+                return callback(err.message);
+            } else {
+                return db.query("UPDATE customers SET fname = ?, lname = ?, password = ? WHERE username = ?",
+                    [newC.name, newC.lname, hashedPassword], callback);
+            }
+        });
+    },
+
+    updatePassword(un, newC, callback){
+        bcrypt.hash(newC.password, saltrounds, function(err, hashedPassword){
+            if(err){
+                return callback(err.message);
+            }  else{
+                return db.query("UPDATE customers SET password = ? WHERE username = ?",
+                    [hashedPassword, un], callback);
+            }
+        });
+    },
+
+    delete(un, callback){
+        return db.query("DELETE FROM customers WHERE username = ?", [un], callback);
+    }
 }
 
 module.exports = customers;
